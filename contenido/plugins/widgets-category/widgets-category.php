4e4f5e0aeb97b0e5f235c878d47ca87b70031394
coco <cocodelacueva@gmail.com>
2016-11-09 15:44:38 -0300

    cambios pedidos por redbio: 1. no hay más publicaciones 2.modifiqué las categorias 3. modifiqué textos (historias, vision objetivos, msj de bienvenida) 4. y modifiqué el pluggin borrando la parte del widget de publicaciones 5. y modifiqué la pagina de inicio, 6. guardé la base de datos

--- contenido/plugins/widgets-category/widgets-category.php
+++ contenido/plugins/widgets-category/widgets-category.php
@@ -1,7 +1,7 @@
 <?php
 /* Plugin Name: Widgets Categorys
 Description: Sirve para mostrar cierta categorias de entradas y no otras.
-Version: 1.0
+Version: 1.1
 Author: LaCueva.tv
 Author URI: http://lacueva.tv
 License: GPL2
@@ -94,111 +94,6 @@ class lqva_widgets_category extends WP_Widget {
                 'post_type'      => 'post',
                 'posts_per_page' => intval( $instance['limit'] ),
                 'cat' => '8,5',
-            )
-        );
-        // The Loop
-        if ( $project_query->have_posts() ) : ?>
-
-            <?php while ( $project_query->have_posts() ) : ?>
-                
-                <ul class="item">
-                <?php $project_query->the_post(); ?>
-                
-                    <li>
-                    <?php the_title( '<a href="' . get_the_permalink() . '">', '</a>' ); ?>
-                    <div class="thumbnail-post">
-                        <?php
-                            if ( $instance['show_thumbnail'] ) :
-                                the_post_thumbnail();
-                            endif;
-                        ?>
-                    </li>
-                </ul>
-
-            <?php endwhile; ?>
-
-            <?php wp_reset_postdata();
-        else :
-            echo esc_html__( 'No se encontraron post', 'lang' );
-        endif;
-        echo $args['after_widget'];
-    }
-}
-
-
-class lqva_widgets_category_publicaciones extends WP_Widget {
-    /**
-     * Sets up the widgets name etc
-     */
-    public function __construct() {
-        $widget_options = array(
-            'description'    => esc_html__( 'Muestra las ultimas publicaciones', 'lang' ),
-            'title'          => esc_html__( 'Publicaciones' ),
-            'limit'          => 3,
-            'show_thumbnail' => true,
-            'category'       => '1',
-        );
-        parent::__construct( 'lqva-widgets-category-publicaciones', esc_html__( 'lqva - Publicaciones recientes', 'lang' ), $widget_options );
-    }
-    /**
-     * Outputs the options form on admin
-     *
-     * @param array $instance The widget options
-     *
-     * @return void
-     */
-    public function form( $instance ) {
-        $title          = ! empty( $instance['title'] ) ? $instance['title'] : $this->widget_options['title'];
-        $limit          = ! empty( $instance['limit'] ) ? $instance['limit'] : $this->widget_options['limit'];
-        $show_thumbnail = ! empty( $instance['show_thumbnail'] ) ? true : false;
-        ?>
-
-        <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Título Widget', 'lang' ); ?>:</label><br />
-            <input id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-        </p>
-        <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>"><?php esc_html_e( 'Número de entrdas a mostrar', 'lang' ); ?></label><br />
-            <input id="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'limit' ) ); ?>" type="text" value="<?php echo esc_attr( $limit ); ?>">
-        </p>
-        <p>
-            <input id="<?php echo esc_attr( $this->get_field_id( 'show_thumbnail' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_thumbnail' ) ); ?>" type="checkbox" value="true" <?php checked( $show_thumbnail, false ); ?>>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'show_thumbnail' ) ); ?>"><?php esc_html_e( 'Mostrar imagen destacada', 'lang' ); ?></label>
-        </p>
-
-        <?php
-    }
-    /**
-     * Processing widget options on save.
-     *
-     * @param array $new_instance The new options
-     * @param array $old_instance The previous options
-     *
-     * @return array
-     */
-    public function update( $new_instance, $old_instance ) {
-        $instance = array(
-            'title'          => isset( $new_instance['title'] ) ? $new_instance['title'] : '',
-            'limit'          => isset( $new_instance['limit'] ) ? intval( $new_instance['limit'] ) : 0,
-            'show_thumbnail' => isset( $new_instance['show_thumbnail'] ) ? boolval( $new_instance['show_thumbnail'] ) : false,
-        );
-        return $instance;
-    }
-    /**
-     * Outputs the content of the widget
-     *
-     * @param array $args
-     * @param array $instance
-     */
-    public function widget( $args, $instance ) {
-        echo $args['before_widget'];
-        if ( ! empty( $instance['title'] ) ) {
-            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
-        }
-        $project_query = new WP_Query( array(
-                'post_type'      => 'post',
-                'posts_per_page' => intval( $instance['limit'] ),
-                'cat' => '10',
             )
         );
         // The Loop
